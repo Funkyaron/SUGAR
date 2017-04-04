358108780dc796c05587b5c03de226ada578d15e
Funkyaron <aron.proebsting@student.uni-siegen.de>
2017-04-04 17:16:45 +0200

    UpdateProfileReceiver provisorium

--- app/src/main/java/com/example/peter/sugar/UpdateProfileReceiver.java
+++ app/src/main/java/com/example/peter/sugar/UpdateProfileReceiver.java
@@ -1,17 +1,41 @@
 package com.example.peter.sugar;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 
 /**
- * Created by Peter on 04.04.2017.
+ * Created by Funkyaron on 04.04.2017.
  */
 
 class UpdateProfileReceiver extends BroadcastReceiver {
 
+    private static final int ID = 42;
+
     @Override
     public void onReceive(Context context, Intent intent) {
 
+
+        /* Temporarily coded to test the alarm functionality */
+
+        Bundle extras = intent.getExtras();
+        String name = extras.getString(AlarmInitializer.PROFILE_NAME_KEY);
+        Boolean active = extras.getBoolean(AlarmInitializer.ACTIVE_KEY);
+
+        Notification.Builder builder = new Notification.Builder(context);
+
+        builder.setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(name)
+                .setContentText(active.toString())
+                .setWhen(System.currentTimeMillis());
+
+        Notification noti = builder.build();
+
+        NotificationManager notiMgr = (NotificationManager)
+                context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notiMgr.notify(ID, noti);
     }
 }
