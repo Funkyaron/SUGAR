a0933854d143ba26e9c12012ac4927d0935fcb2e
Peter Safontschik <psafontschik@googlemail.com>
2017-04-29 19:55:27 +0200

    First try to implement a download system to retrieve files from FTP servers

--- app/src/main/java/com/example/peter/sugar/XMLProfileUpdater.java
+++ app/src/main/java/com/example/peter/sugar/XMLProfileUpdater.java
@@ -1,45 +1,56 @@
 package com.example.peter.sugar;
-/*
- import org.apache.commons.net.ftp.*;
-import java.io.IOException;
-import java.io.FileOutputStream;
-import java.io.FileInputStream;
+
+import android.content.Context;
+import android.util.Log;
+
+import org.apache.commons.net.ftp.*;
+import java.io.*;
 
 /**
  * Created by Peter on 19.04.2017.
- *//*
+ */
 
-public class XMLProfileUpdater{
+class XMLProfileUpdater{
 
-    public static String[] display_files ( String host, int port, String usr, String pwd ) throws IOException
+    Context context;
+    private FTPClient androidClient;
+    private int reply;
+    String gnuTest = "https://ftp.gnu.org";
+
+    /**
+     * Downloads the profile from the FTP-Server. Note that the app only downloads from the public
+     * GNU server to test the Apache Commons Net library.
+     * @throws IOException is thrown if the profile file is not located in the FRP
+     * @throws FTPConnectionClosedException is thrown if the connection can't be established
+     */
+    public void downloadProfile() throws IOException, FTPConnectionClosedException
     {
-        FTPClient client = new FTPClient();
-        String files[];
-        try {
-            client.connect(host,port);
-            client.login(usr,pwd);
-            files = client.listNames();
-            client.logout();
-        } finally {
-            client.disconnect();
+        androidClient.connect(gnuTest);
+        androidClient.enterLocalPassiveMode();
+        reply = androidClient.getReplyCode();
+        androidClient.setFileType(FTP.BINARY_FILE_TYPE);
+
+        if( !FTPReply.isPositiveCompletion(reply))
+        {
+            androidClient.disconnect();
+            Log.d(MainActivity.LOG_TAG,"XMLProfileUpdater : Bad reply code!");
+        }
+        else
+        {
+            String remoteFilePath = "/gnu/GNUinfo/README";
+            String fileName = "README";
+            InputStream source = androidClient.retrieveFileStream(remoteFilePath);
+            FileOutputStream target = new FileOutputStream(context.getFilesDir() + fileName);
+            byte buffer[] = new byte[4096];
+            long count = 0L;
+            int n = 0;
+            while( -1 != (n = source.read(buffer)))
+            {
+                target.write(buffer,0,n);
+                count = count + n;
+            }
         }
 
-        return files;
     }
-
-    public static boolean download_file ( String output_path, String remote_path, boolean message) throws IOException
-    {
-        FTPClient client = new FTPClient();
-        FileOutputStream downloaded_file = null;
-        boolean error = false;
-
-        try {
-            client.connect("RandomClient",22);
-
-        } finally {
-
-        }
-        return true;
-    }
-}*/
+}
 
