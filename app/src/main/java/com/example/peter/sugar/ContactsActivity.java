3f25c626a19738a06b7b5238a986d85a3b5fa6a2
Funkyaron <aron.proebsting@student.uni-siegen.de>
2017-05-05 14:24:52 +0200

    ContactsActivity Update 1

--- app/src/main/java/com/example/peter/sugar/ContactsActivity.java
+++ app/src/main/java/com/example/peter/sugar/ContactsActivity.java
@@ -1,13 +1,72 @@
 package com.example.peter.sugar;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class ContactsActivity extends AppCompatActivity {
+public class ContactsActivity extends AppCompatActivity
+    implements ActivityCompat.OnRequestPermissionsResultCallback {
+
+    /**
+     * Request code to identify the request for contacts permissions.
+     */
+    private final int REQUEST_CONTACTS = 1;
+
+    /**
+     * Permissions we need to read and write contacts.
+     */
+    private final String[] PERMISSION_CONTACTS = {Manifest.permission.READ_CONTACTS,
+        Manifest.permission.WRITE_CONTACTS};
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+
+        if(ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_CONTACTS) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, PERMISSION_CONTACTS, REQUEST_CONTACTS);
+        }
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults)
+    {
+        if (requestCode == REQUEST_CONTACTS)
+        {
+            if(verifyPermissions(grantResults))
+                Toast.makeText(this, "Berechtigungen genehmigt", Toast.LENGTH_LONG).show();
+            else
+            {
+                Toast.makeText(this, "Berechtigungen nicht genehmigt", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
+        else
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private boolean verifyPermissions(int[] grantResults)
+    {
+        if (grantResults.length < 1)
+            return false;
+
+        for (int result : grantResults)
+        {
+            if (result != PackageManager.PERMISSION_GRANTED)
+                return false;
+        }
+
+        return true;
     }
 }
