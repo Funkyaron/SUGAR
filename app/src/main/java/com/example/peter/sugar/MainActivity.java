cd38ecc68e61737dfe635795903a76ae2c8dd33e
Peter Safontschik <psafontschik@googlemail.com>
2017-06-27 10:12:38 +0200

    jfzstrduzf

--- app/src/main/java/com/example/peter/sugar/MainActivity.java
+++ app/src/main/java/com/example/peter/sugar/MainActivity.java
@@ -13,7 +13,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -46,7 +48,12 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        numbersView = (TextView) findViewById(R.id.numbers_view);
+       // numbersView = (TextView) findViewById(R.id.numbers_view);
+
+        String[] adapterContent = { "Hallo","Welt" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,adapterContent);
+        ListView list = (ListView) findViewById(R.id.list);
+        list.setAdapter(adapter);
 
         // Concerning runtime permissions
         if (ActivityCompat.checkSelfPermission(this,
@@ -59,72 +66,6 @@ public class MainActivity extends AppCompatActivity
         } else {
             Log.d(LOG_TAG, "Permissions granted");
         }
-
-        Log.d(LOG_TAG, "getFilesDir(): " + getFilesDir());
-
-
-        String name = "Testprofile";
-        boolean[] days = {
-            true,
-            true,
-            false,
-            true,
-            true,
-            false,
-            false
-        };
-        TimeObject[] start = {
-                new TimeObject(9,30),
-                new TimeObject(10,5),
-                new TimeObject(0,0),
-                new TimeObject(11,13),
-                new TimeObject(8,0),
-                new TimeObject(0,0),
-                new TimeObject(0,0)
-        };
-        TimeObject[] end = {
-                new TimeObject(18,0),
-                new TimeObject(13,13),
-                new TimeObject(0,0),
-                new TimeObject(16,30),
-                new TimeObject(20,0),
-                new TimeObject(0,0),
-                new TimeObject(0,0)
-        };
-        ArrayList<String> numbers = new ArrayList<>(0);
-
-        mProfile = new Profile(name, days, start, end, numbers);
-
-        try {
-            mProfile.saveProfile(this);
-        } catch (Exception e) {
-            Log.e(LOG_TAG, "Save test profile: " + e.toString());
-        }
-
-        numbersView.setText(mProfile.toString());
-
-        TimeManager.updateProfileStatus(this, mProfile);
-
-
-
-
-
-        Button contactsButton = (Button) findViewById(R.id.contacts_button_id);
-        contactsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                DialogFragment newFragment = new ContactsDialogFragment();
-                Bundle args = new Bundle(1);
-                args.putString(KEY_PROFILE_NAME, mProfile.getName());
-                newFragment.setArguments(args);
-                try {
-                    newFragment.show(getFragmentManager(), "contacts");
-                } catch (Exception e) {
-                    Log.e(LOG_TAG, "MainActivity: " + e.toString());
-                }
-            }
-        });
-
     }
 
     @Override
@@ -169,15 +110,6 @@ public class MainActivity extends AppCompatActivity
         }
 
         return true;
-    }
-
-    private void logBlockList(BlockList blockList) {
-        ArrayList<String> blockedNumbers = blockList.getBlockedNumbers();
-        Log.d(LOG_TAG, "That's the blocklist:");
-
-        for (String number : blockedNumbers) {
-            Log.d(LOG_TAG, number);
-        }
     }
 
 }
