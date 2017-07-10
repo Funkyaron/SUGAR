7cd43a15ab17c0a1cc4ffedca471a90826bacaa5
Peter Safontschik <psafontschik@googlemail.com>
2017-07-10 09:01:37 +0200

    Changelog: "DisplayProfileActivity.java" -> Added the class definition and started working on the responsiveness

---
+++ app/src/main/java/com/example/peter/sugar/DisplayProfileActivity.java
@@ -0,0 +1,28 @@
+package com.example.peter.sugar;
+
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Peter on 09.07.2017.
+ */
+
+class DisplayProfileActivity extends AppCompatActivity
+{
+
+    Bundle passedProfile;
+
+    protected void onCreate(Bundle savedInstances)
+    {
+        super.onCreate(savedInstances);
+        setContentView(R.layout.displayprofileactivity);
+    }
+
+    protected void displayContactsChooser()
+    {
+        android.app.FragmentTransaction testTransaction = getFragmentManager().beginTransaction();
+        ContactsDialogFragment testFragment = new ContactsDialogFragment();
+    }
+}
