37f3d19cf1aeede6cfcc720c5cab5f189a5191a1
softwaredev101 <email>
2017-09-05 07:29:32 +0200

    Fixed bug : Timetable had an empty ArrayList<TableRow> which is now filled upon initalization with seven empty TableRows which already contain the structure of the table ( Left : Weekday, Right: Time ).

--- app/src/main/java/com/example/peter/sugar/EditProfileActivity.java
+++ app/src/main/java/com/example/peter/sugar/EditProfileActivity.java
@@ -2,6 +2,8 @@ package com.example.peter.sugar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class EditProfileActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class EditProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        LinearLayout ll = (LinearLayout) findViewById(R.id.edit_layout);
+        TextView profileName = (TextView) findViewById(R.id.profile_name);
+        profileName.setText(getIntent().getExtras().getString("profileName"));
+        TimeTable testTable = new TimeTable(this);
+        try {
+            testTable.convertProfileToTimeTable(Profile.readProfileFromXmlFile("Kurz",this));
+        } catch ( Exception e ) {
+            e.printStackTrace();
+        }
+        ll.addView(testTable);
     }
 }
--- app/src/main/java/com/example/peter/sugar/TimeTable.java
+++ app/src/main/java/com/example/peter/sugar/TimeTable.java
@@ -6,6 +6,10 @@ package com.example.peter.sugar;
 
 import java.util.*;
 import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.ViewGroup;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -16,19 +20,49 @@ public class TimeTable extends TableLayout
     private ArrayList<TableRow> timeTableRows;
     private TableRow toBeModifiedTableRow;
     private TableRow toBeAddedTableRow;
-    private int weekDayCount = 0;
 
     public TimeTable(Context context)
     {
         super(context);
+        this.context = context;
         timeTableRows = new ArrayList<TableRow>(7);
+        for(int tableRowCounter = 0; tableRowCounter < 7; tableRowCounter++ )
+        {
+            Log.d(MainActivity.LOG_TAG,"Added a new tableRow to the TableLayout!");
+            TableRow test = new TableRow(context);
+            timeTableRows.add(test);
+        }
         toBeModifiedTableRow = new TableRow(context);
         toBeAddedTableRow = new TableRow(context);
     }
 
-    public void profileToTimeTable(Profile profile)
+    public void convertProfileToTimeTable(Profile passedProfile) throws Exception
     {
-        TimeObject[] startTimes = profile.getStart();
-        TimeObject[] endTimes = profile.getEnd();
+        TimeObject[] startTimes = passedProfile.getStart();
+        TimeObject[] endTimes = passedProfile.getEnd();
+        boolean[] activatedDays = passedProfile.getDays();
+
+        // Setup the ArrayList of TableRow
+        for(ListIterator<TableRow> rowIterator = timeTableRows.listIterator();rowIterator.hasNext();)
+        {
+            Log.d(MainActivity.LOG_TAG,"Another one!");
+            // Setup TableRow's properties
+            TableRow currRow = rowIterator.next();
+            currRow.setLayoutParams(new TableLayout.LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.WRAP_CONTENT));
+            currRow.setWeightSum(6.0f);
+
+            // Setup TextView's properties
+            TextView currWeekDay = new TextView(context);
+            currWeekDay.setLayoutParams(new TableRow.LayoutParams(0,TableRow.LayoutParams.WRAP_CONTENT,3.0f));
+            currWeekDay.setBackgroundColor(Color.RED);
+            TextView currWeekDayTime = new TextView(context);
+            currWeekDayTime.setLayoutParams(new TableRow.LayoutParams(0,TableRow.LayoutParams.WRAP_CONTENT,3.0f));
+            currWeekDayTime.setBackgroundColor(Color.GREEN);
+
+            // Add TextView to the current TableRow
+            currRow.addView(currWeekDay);
+            currRow.addView(currWeekDayTime);
+            addView(currRow);
+        }
     }
 }
