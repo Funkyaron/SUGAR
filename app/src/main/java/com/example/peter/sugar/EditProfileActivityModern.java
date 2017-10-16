61b6e8976e026cb7d70c352aa7394faccb69e2f9
softwaredev101 <email>
2017-10-16 14:16:41 +0200

    New functions : Fixed some bugs 2

--- app/src/main/java/com/example/peter/sugar/EditProfileActivityModern.java
+++ app/src/main/java/com/example/peter/sugar/EditProfileActivityModern.java
@@ -12,6 +12,7 @@ import android.widget.CompoundButton;
 import android.widget.NumberPicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
@@ -39,10 +40,15 @@ public class EditProfileActivityModern extends AppCompatActivity
     private Button confirmUpdatedTime;
     private NumberPicker chooseHour;
     private NumberPicker chooseMinute;
-    private int selectedDay = 1;
+    private int selectedDay = -1;
     private Context activityContext;
-    private int selectedWeekDayIndex;
+    private int selectedWeekDayIndex = -1;
 
+    /**
+     * In this function every variable which is declared at the beginning of the file is given an inital value and crucial operations like
+     * the registration of all listeners are done.
+     * @param savedInstances
+     */
     @Override
     public void onCreate(Bundle savedInstances)
     {
@@ -98,8 +104,25 @@ public class EditProfileActivityModern extends AppCompatActivity
                 if( isChecked && !endQuestion.isChecked() )
                 {
                     beginOrEnd = 's';
+                    if( selectedWeekDayIndex >= 0) {
+                        chooseHour.setValue(chosenProfile.getStart()[selectedWeekDayIndex].getHour());
+                        chooseMinute.setValue(chosenProfile.getStart()[selectedWeekDayIndex].getMinute());
+                    }
                 } else if ( isChecked && endQuestion.isChecked() ) {
                     beginOrEnd = 'n';
+                    chooseHour.setValue(0);
+                    chooseMinute.setValue(0);
+                } else if ( !isChecked && !endQuestion.isChecked())
+                {
+                    beginOrEnd = 'n';
+                    chooseHour.setValue(0);
+                    chooseMinute.setValue(0);
+                } else if ( !isChecked && endQuestion.isChecked() ) {
+                    beginOrEnd = 'e';
+                    if( selectedWeekDayIndex >= 0 ) {
+                        chooseHour.setValue(chosenProfile.getEnd()[selectedWeekDayIndex].getHour());
+                        chooseMinute.setValue(chosenProfile.getEnd()[selectedWeekDayIndex].getMinute());
+                    }
                 }
             }
         });
@@ -109,13 +132,37 @@ public class EditProfileActivityModern extends AppCompatActivity
                 if( isChecked && !startQuestion.isChecked() )
                 {
                     beginOrEnd = 'e';
+                    if( selectedWeekDayIndex >= 0 ) {
+                        chooseHour.setValue(chosenProfile.getEnd()[selectedWeekDayIndex].getHour());
+                        chooseMinute.setValue(chosenProfile.getEnd()[selectedWeekDayIndex].getMinute());
+                    }
                 } else if ( isChecked && startQuestion.isChecked() ) {
                     beginOrEnd = 'n';
+                    chooseHour.setValue(0);
+                    chooseMinute.setValue(0);
+                } else if ( !isChecked && !startQuestion.isChecked() )
+                {
+                    beginOrEnd = 'n';
+                    chooseHour.setValue(0);
+                    chooseMinute.setValue(0);
+                } else if ( !isChecked && startQuestion.isChecked() ) {
+                    beginOrEnd = 's';
+                    if( selectedWeekDayIndex >= 0 ) {
+                        chooseHour.setValue(chosenProfile.getStart()[selectedWeekDayIndex].getHour());
+                        chooseMinute.setValue(chosenProfile.getStart()[selectedWeekDayIndex].getMinute());
+                    }
                 }
             }
         });
     }
 
+    /**
+     * Registers all button listeners in this activity. There are many possible cases which can occur and have to be worked on.
+     * Case A : One wants to set the start time of a valid day => Update the old time with the new one
+     * Case B : One wants to set the end time of a valid day => Update the old time with the new one
+     * Case C : One wants to set the start time for an invalid day => Error Toast
+     * Case D : One wants to set the end time for an invalid day => Error Toast
+     */
     public void registerButtonListener()
     {
         confirmUpdatedTime.setOnClickListener(new View.OnClickListener() {
@@ -127,19 +174,32 @@ public class EditProfileActivityModern extends AppCompatActivity
 
                     if( beginOrEnd == 's' )
                     {
-                        chosenProfile.setStartForDay(selectedDay,updatedTime);
-                        Log.d(MainActivity.LOG_TAG,"You have changed the time to " + chosenProfile.getStart()[selectedDay].toString());
-                        try {
-                            chosenProfile.saveProfile(activityContext);
-                        } catch ( Exception e ) {
-                            e.printStackTrace();
+                        if( selectedWeekDayIndex >= 0) {
+                            if (chosenProfile.getStart()[selectedWeekDayIndex].getHour() >= 0) {
+                                chosenProfile.setStartForDay(selectedDay, updatedTime);
+                                Log.d(MainActivity.LOG_TAG, "You have changed the time to " + chosenProfile.getStart()[selectedDay].toString());
+                                try {
+                                    chosenProfile.saveProfile(activityContext);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            } else if( chosenProfile.getStart()[selectedWeekDayIndex].getHour() == -1 ){
+                                Toast.makeText(activityContext, "Leider ist dieser Tag blockiert!", Toast.LENGTH_SHORT).show();
+                            }
                         }
                     } else if ( beginOrEnd == 'e' ) {
-                        chosenProfile.setEndForDay(selectedDay, updatedTime);
-                        try {
-                            chosenProfile.saveProfile(activityContext);
-                        } catch ( Exception e ) {
-                            e.printStackTrace();
+                        if( selectedWeekDayIndex >= 0) {
+                            if (chosenProfile.getEnd()[selectedWeekDayIndex].getHour() >= 0) {
+                                Log.d(MainActivity.LOG_TAG,"You have changed the time to " + chosenProfile.getEnd()[selectedWeekDayIndex].toString());
+                                chosenProfile.setEndForDay(selectedDay, updatedTime);
+                                try {
+                                    chosenProfile.saveProfile(activityContext);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            } else if(chosenProfile.getEnd()[selectedWeekDayIndex].getHour() == -1){
+                                Toast.makeText(activityContext, "Leider ist dieser Tag blockiert!", Toast.LENGTH_SHORT).show();
+                            }
                         }
                     }
 
@@ -147,6 +207,11 @@ public class EditProfileActivityModern extends AppCompatActivity
         });
     }
 
+    /**
+     * Register all listeners for the "weekViews". If a day is blocked by the profile, it will be displayed with a red background color.
+     * If the opposite its the case, the color will be green. If the user chooses one valid day, the background color of the
+     * corresponding TextView will be green until the user wants to choose another day.
+     */
     public void registerWeekDayLongClickListener()
     {
         for(int currentWeekDay = 0; currentWeekDay < weekDayRow.length; currentWeekDay++ )
@@ -155,22 +220,27 @@ public class EditProfileActivityModern extends AppCompatActivity
             weekDayRow[currentWeekDay].setOnLongClickListener(new View.OnLongClickListener() {
                 public boolean onLongClick(View weekDayView) {
                     for (int blockedWeekDay = 0; blockedWeekDay < 7; blockedWeekDay++) {
-                        if (chosenProfile.getStart()[currDay].getHour() != -1) {
+                        if (chosenProfile.getStart()[currDay].getHour() >= 0 && currDay >= 0) {
                             Log.d(MainActivity.LOG_TAG,"This weekDay should now be blocked!");
-                            weekDayView.setBackgroundResource(R.drawable.weekday_blocked);
-                            chosenProfile.setStartForDay(currDay,new TimeObject(-1,-1));
-                            chosenProfile.setEndForDay(currDay,new TimeObject(-1,-1));
+                            weekDayRow[currDay].setBackgroundResource(R.drawable.weekday_blocked);
+                            TimeObject noAccessObject = new TimeObject(-1,-1);
+                            chosenProfile.setStartForDay(currDay,noAccessObject);
+                            chosenProfile.setEndForDay(currDay,noAccessObject);
+                            Log.d(MainActivity.LOG_TAG,"New start time : " + chosenProfile.getStart()[currDay].toString());
+                            Log.d(MainActivity.LOG_TAG,"New end time : " + chosenProfile.getEnd()[currDay].toString());
                             try {
                                 chosenProfile.saveProfile(activityContext);
                             } catch ( Exception e ) {
                                 e.printStackTrace();
                             }
                             return true;
-                        } else if ( chosenProfile.getStart()[currDay].getHour() == -1){
+                        } else if ( chosenProfile.getStart()[currDay].getHour() == -1 && currDay >= 0 ){
                             Log.d(MainActivity.LOG_TAG,"This weekDay is now unblocked!");
-                            weekDayView.setBackgroundResource(R.drawable.weekday_deactivated);
+                            weekDayRow[currDay].setBackgroundResource(R.drawable.weekday_deactivated);
                             chosenProfile.setStartForDay(currDay,new TimeObject(23,59));
                             chosenProfile.setEndForDay(currDay,new TimeObject(23,59));
+                            Log.d(MainActivity.LOG_TAG,"New start time : " + chosenProfile.getStart()[currDay].toString());
+                            Log.d(MainActivity.LOG_TAG,"New end time : " + chosenProfile.getEnd()[currDay].toString());
                             try {
                                 chosenProfile.saveProfile(activityContext);
                             } catch ( Exception e ) {
@@ -195,14 +265,32 @@ public class EditProfileActivityModern extends AppCompatActivity
                {
                    selectedDay = currDay;
                        Log.d(MainActivity.LOG_TAG, "You have selected the day " + selectedDay);
-                       weekDayView.setBackgroundResource(R.drawable.weekday_activated);
-                       chooseHour.setValue(chosenProfile.getStart()[currDay].getHour());
-                       chooseMinute.setValue(chosenProfile.getStart()[currDay].getMinute());
+                        if( chosenProfile.getStart()[selectedDay].getHour() >= 0 ) {
+                            weekDayView.setBackgroundResource(R.drawable.weekday_activated);
+                        }
+                        if( beginOrEnd == 's') {
+                            chooseHour.setValue(chosenProfile.getStart()[currDay].getHour());
+                            chooseMinute.setValue(chosenProfile.getStart()[currDay].getMinute());
+                        } else if ( beginOrEnd == 'e') {
+                            chooseHour.setValue(chosenProfile.getEnd()[currDay].getHour());
+                            chooseMinute.setValue(chosenProfile.getEnd()[currDay].getMinute());
+                        } else if ( beginOrEnd == 'n' ) {
+                            chooseHour.setValue(0);
+                            chooseMinute.setValue(0);
+                        }
                        for (int anyOtherWeekDay = 0; anyOtherWeekDay < weekDayRow.length; anyOtherWeekDay++) {
                            if ( chosenProfile.getStart()[currDay].getHour() == -1 ) {
-                               weekDayRow[currDay].setBackgroundResource(R.drawable.weekday_blocked);
+                               if(selectedWeekDayIndex >= 0) {
+                                   if( chosenProfile.getStart()[selectedWeekDayIndex].getHour() >= 0 ) {
+                                       weekDayRow[selectedWeekDayIndex].setBackgroundResource(R.drawable.weekday_deactivated);
+                                   }
+                                   weekDayRow[currDay].setBackgroundResource(R.drawable.weekday_blocked);
+                                   selectedWeekDayIndex = currDay;
+                               }
+                               chooseHour.setValue(0);
+                               chooseMinute.setValue(0);
                            } else if ( chosenProfile.getStart()[currDay].getHour() >= 0 ) {
-                               if( selectedWeekDayIndex >= 0) {
+                               if( selectedWeekDayIndex >= 0 && chosenProfile.getStart()[selectedWeekDayIndex].getHour() >= 0) {
                                    weekDayRow[selectedWeekDayIndex].setBackgroundResource(R.drawable.weekday_deactivated);
                                }
                                weekDayRow[currDay].setBackgroundResource(R.drawable.weekday_activated);
